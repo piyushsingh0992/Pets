c7d924f593e16e52e68581e07d947e47c671b8e8
piyushsinghhsv <piyush.singh@hsv.digital>
2021-08-02 01:24:02 +0530

    added test to theme reducer

--- src/contexts/themeContext/theme.test.js
+++ src/contexts/themeContext/theme.test.js
@@ -4,29 +4,27 @@ import { day, night } from "./data.js";
 test("Change to dark theme", () => {
   const initialState = day;
   const action = { payload: "night" };
-
-  localStorage.setItem("activetheme", JSON.stringify({ payload: "night" }));
-  expect(localStorage.getItem("activetheme")).toEqual(
-    JSON.stringify({ payload: "night" })
-  );
-
   let result = themeManager(initialState, action);
   const expectedState = night;
+  expect(localStorage.setItem).toHaveBeenLastCalledWith(
+    "activetheme",
+    JSON.stringify({ payload: "night" })
+  );
 
   expect(result).toStrictEqual(expectedState);
 });
 
 test("Change to day theme", () => {
   const initialState = night;
+
   const action = { payload: "day" };
-
-  localStorage.setItem("activetheme", JSON.stringify({ payload: "day" }));
-  expect(localStorage.getItem("activetheme")).toEqual(
-    JSON.stringify({ payload: "day" })
-  );
-
   let result = themeManager(initialState, action);
   const expectedState = day;
+
+  expect(localStorage.setItem).toHaveBeenLastCalledWith(
+    "activetheme",
+    JSON.stringify({ payload: "day" })
+  );
 
   expect(result).toStrictEqual(expectedState);
 });
